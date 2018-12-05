f8fa29658c592f062db89556963f1635b7fe9f7a
SomeTake <tp1002217g@gmail.com>
2018-12-05 14:40:38 +0900

    Script

---
+++ SoundGame/Assets/CSVWriter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;  // <- ここに注意
+
+public class CSVWriter : MonoBehaviour
+{
+
+    public string fileName; // 保存するファイル名
+
+    //テスト用
+    //  void Start () {
+    //      WriteCSV ("Hello,World");
+    //  }
+
+    // CSVに書き込む処理
+    public void WriteCSV(string txt)
+    {
+        StreamWriter streamWriter;
+        FileInfo fileInfo;
+        fileInfo = new FileInfo(Application.dataPath + "/" + fileName + ".csv");
+        streamWriter = fileInfo.AppendText();
+        streamWriter.WriteLine(txt);
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+}
---
+++ SoundGame/Assets/GameUtil.cs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameUtil
+{
+
+    public static KeyCode GetKeyCodeByLineNum(int lineNum)
+    {
+        switch (lineNum)
+        {
+            case 0:
+                return KeyCode.D;
+            case 1:
+                return KeyCode.F;
+            case 2:
+                return KeyCode.J;
+            case 3:
+                return KeyCode.K;
+            default:
+                return KeyCode.None;
+        }
+    }
+}
