3f246293dd3f4e1711acac912a4564a449664e48
SomeTake <tp1002217g@gmail.com>
2018-12-05 19:54:46 +0900

    CSV create
    音楽の再生成功、CSV書き出し失敗

---
+++ MusicGame/Assets/CSVWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;  // <- ここに注意
+
+public class CSVWriter : MonoBehaviour
+{
+
+    public string fileName = "Resources/CSV/TestTiming"; // 保存するファイル名
+
+    ////テスト用
+    //void Start()
+    //{
+    //    WriteCSV("Hello,World");
+    //}
+
+    // CSVに書き込む処理
+    public void WriteCSV(string txt)
+    {
+        StreamWriter streamWriter;
+        FileInfo fileInfo;
+        fileInfo = new FileInfo(Application.dataPath + "/" + fileName + ".csv");
+        streamWriter = fileInfo.AppendText();
+        streamWriter.WriteLine(txt);
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+}
--- MusicGame/Assets/NotesTimingMaker.cs
+++ MusicGame/Assets/NotesTimingMaker.cs
@@ -6,23 +6,74 @@ public class NotesTimingMaker : MonoBehaviour
 {
 
     private AudioSource _audioSource;
+    private float _startTime = 0;
+    private CSVWriter _CSVWriter;
 
-    public GameObject StartButton;
+    private bool _isPlaying = false;
+    public GameObject startButton;
 
+    // 一度のみ発生
     void Start()
     {
+        // GameMusicオブジェクトを探す
         _audioSource = GameObject.Find("GameMusic").GetComponent<AudioSource>();
-
+        // CSVWriterオブジェクトを探す
+        _CSVWriter = GameObject.Find("CSVWriter").GetComponent<CSVWriter>();
     }
 
+    // 毎フレーム発生
     void Update()
     {
-
+        if (_isPlaying)
+        {
+            DetectKeys();
+        }
     }
 
     public void StartMusic()
     {
-        StartButton.SetActive(false);
+        startButton.SetActive(false);
         _audioSource.Play();
+        _startTime = Time.time;
+        _isPlaying = true;
+    }
+
+    void DetectKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            WriteNotesTiming(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            WriteNotesTiming(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WriteNotesTiming(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            WriteNotesTiming(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            WriteNotesTiming(4);
+        }
+    }
+
+    void WriteNotesTiming(int num)
+    {
+        Debug.Log(GetTiming());
+        _CSVWriter.WriteCSV(GetTiming().ToString() + "," + num.ToString());
+    }
+
+    float GetTiming()
+    {
+        return Time.time - _startTime;
     }
 }
