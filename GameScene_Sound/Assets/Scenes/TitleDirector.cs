80465a379104fe1818dd9bbd30f4102cfd34872b
SomeTake <tp1002217g@gmail.com>
2018-12-06 16:53:43 +0900

    SceneDirector
    シーン遷移完成

---
+++ GameScene_Sound/Assets/Scenes/TitleDirector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // シーン遷移に必要
+
+public class TitleDirector : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+	}
+}
